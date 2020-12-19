ee49b4a4ced3d2a2367ebd9f27ae0ab34baef370
Falak Chandni <falak.chandni08@gmail.com>
2020-12-19 17:12:46 +0530

    Score added

--- Pig.js
+++ Pig.js
@@ -6,7 +6,8 @@ class Pig extends BaseClass {
   }
 
  display(){
-   console.log(this.body.speed);
+   //console.log(this.body.speed);
+   //console.log(this.Visiblity);
    if(this.body.speed < 3){
     super.display();
    }
@@ -14,11 +15,23 @@ class Pig extends BaseClass {
      World.remove(world, this.body);
      push();
      this.Visiblity = this.Visiblity - 5;
+   
+
      tint(255,this.Visiblity);
      image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+
      pop();
    }
    
+ }
+
+ score(){
+   if(this.Visiblity < 0 && this.Visiblity > -1005){
+    // console.log("message");
+
+    score++;
+
+   }
  }
 
 
